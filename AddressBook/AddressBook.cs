dd6cc2dea122c9939ed8d58eb0a771525b20c9c9
Amartya <amartyaanand21@live.com>
2020-10-04 21:42:54 +0530

    [Amartya] Add : Creating Contact.

---
+++ AddressBook/AddressBook.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBook
+{
+    class AddressBook
+    {
+        public List<Contact> ContactList;
+        public AddressBook()
+        {
+            this.ContactList = new List<Contact>();
+        }
+    }
+}
