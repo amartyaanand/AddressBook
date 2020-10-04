8bbe84b5eeee102353ceea3ad8706c8d5b2e9b22
Amartya <amartyaanand21@live.com>
2020-10-04 21:47:07 +0530

    [Amartya] Add : Adding new contact.

--- AddressBook/AddressBook.cs
+++ AddressBook/AddressBook.cs
@@ -11,5 +11,9 @@ namespace AddressBook
         {
             this.ContactList = new List<Contact>();
         }
+        public void AddContact(Contact contactObj)
+        {
+            this.ContactList.Add(contactObj);
+        }
     }
 }
