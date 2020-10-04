8709a4f4b9edf3f3043204f77b415b78fbdff0f4
Amartya <amartyaanand21@live.com>
2020-10-04 21:58:08 +0530

    [Amartya] Add: Deleting Contact by person's name.

--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -7,11 +7,11 @@ namespace AddressBook
         static void Main(string[] args)
         {
             AddressBook addressBook = new AddressBook();
-            Console.WriteLine("Welcome to Address Book Program.");
+            Console.WriteLine("Welcome To Address Book Program");
             int choice;
             do
             {
-                Console.WriteLine("\nMenu : \n1.Add Contact \n2.Edit Contact \n0.Exit");
+                Console.WriteLine("\nMenu : \n1.Add Contact \n2.Edit Contact \n3.Delete Contact\n0.Exit");
                 choice = Convert.ToInt32(Console.ReadLine());
                 switch (choice)
                 {
@@ -35,6 +35,21 @@ namespace AddressBook
                             SetContactDetails(contact2);
                             addressBook.ContactList[index] = contact2;
                             Console.WriteLine("Contact Updated Successfully");
+                        }
+                        break;
+                    case 3:
+                        Console.WriteLine("Enter the First Name of Contact you wish to delete");
+                        string fname = Console.ReadLine();
+                        int idx = addressBook.FindByFirstName(fname);
+                        if (idx == -1)
+                        {
+                            Console.WriteLine("No Contact Exists with Following First Name");
+                            continue;
+                        }
+                        else
+                        {
+                            addressBook.DeleteContact(idx);
+                            Console.WriteLine("Contact Deleted Successfully");
                         }
                         break;
                 }
